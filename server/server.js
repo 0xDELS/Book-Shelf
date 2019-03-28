937f99e22685766ec736029ee718c0f54436487f
Diogo Silva <diogo@diogo-silva.pt>
2019-03-28 17:39:14 +0000

    Server Initial Configurations

---
+++ server/server.js
@@ -0,0 +1,19 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+const cookieParser = require('cookie-parser');
+const mongoose = require('mongoose');
+const config = require('./config/Config').get(process.env.NODE_ENV);
+const app = express();
+
+//DB
+mongoose.Promise = global.Promise;
+mongoose.connect(config.DATABASE, { useNewUrlParser: true });
+
+//MIDDLEWARES
+app.use(bodyParser.json());
+app.use(cookieParser());
+
+const port = process.env.PORT || 3001;
+app.listen(port, () => {
+    console.log(`Server Running on port: ${port}`);
+})
