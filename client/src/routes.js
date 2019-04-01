b1b33a1796bcc785f25ee1107ea0e56938c0c00b
Diogo Silva <diogo@diogo-silva.pt>
2019-04-01 18:45:52 +0100

    Layout HOC

--- client/src/routes.js
+++ client/src/routes.js
@@ -1,12 +1,15 @@
 import React from 'react';
 import { Switch, Route } from 'react-router-dom';
 import Home from './Components/Home/Home'
+import Layout from './HOC/Layout'
 
 const Routes = () => {
     return (
-        <Switch>
-            <Route path="/" exact component={Home}/>
-        </Switch>
+        <Layout>
+            <Switch>
+                <Route path="/" exact component={Home}/>
+            </Switch>
+        </Layout>
     );
 };
 
