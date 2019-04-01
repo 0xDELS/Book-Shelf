b93a406cec5c6e4b5dc0461dccedfb80cbfbd094
Diogo Silva <diogo@diogo-silva.pt>
2019-04-01 15:05:37 +0100

    Reducers

--- client/src/Reducers/index.js
+++ client/src/Reducers/index.js
@@ -1,7 +1,10 @@
 import { combineReducers } from 'redux';
+import Books from './BooksReducer';
+import Users from './UsersReducer';
 
 const rootReducer = combineReducers({
-
+    Books,
+    Users
 });
 
 export default rootReducer;
