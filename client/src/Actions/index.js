1196441e2d9bd0d40ff52ab47160e29e335c1f08
Diogo Silva <diogo@diogo-silva.pt>
2019-04-02 12:31:50 +0100

    Book Reviews

--- client/src/Actions/index.js
+++ client/src/Actions/index.js
@@ -15,3 +15,37 @@ export function getBooks(limit = 10, start = 0, order = 'asc', list = ''){
         payload: request
     }
 }
+
+export function getBookWithReviewer( id ){
+    const request = axios.get(`/api/getBook?id=${id}`)
+
+    return (dispatch) => {
+        request.then(({data}) => {
+            let book = data
+
+            axios.get(`/api/getReviewer?id=${book.ownerID}`)
+            .then(({data}) => {
+
+                let response = {
+                    book,
+                    reviewer: data
+                }
+
+                dispatch({
+                    type: 'GET_BOOK_WITH_REVIEWER',
+                    payload: response
+                })
+            })            
+        })
+    }
+}
+
+export function clearBookWithReviewer (){
+    return {
+        type:'CLEAR_BOOK_WITH_REVIEWER',
+        payload: {
+            book: {},
+            reviewer: {}
+        }
+    }
+}
--- client/src/routes.js
+++ client/src/routes.js
@@ -2,12 +2,14 @@ import React from 'react';
 import { Switch, Route } from 'react-router-dom';
 import Home from './Components/Home/Home'
 import Layout from './HOC/Layout'
+import BookView from './Components/Books'
 
 const Routes = () => {
     return (
         <Layout>
             <Switch>
                 <Route path="/" exact component={Home}/>
+                <Route path="/books/:id" exact component={BookView}/>
             </Switch>
         </Layout>
     );
